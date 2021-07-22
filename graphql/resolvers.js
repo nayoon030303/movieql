38de8c55362e8ec141d9f79cc56f6ba1e7b2ea08
nayoon030303 <nayoon030303@gmail.com>
2021-07-23 03:30:07 +0900

    Add Mutation error해결

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
         movie:(_,{id})=>getById(id)
     },
     Mutation:{
-        addMovie:(_,{name},{score})=>addMovie(name,score),
+        addMovie:(_,{name,score})=>addMovie(name,score),
         deleteMovie:(_,{id})=>deleteMovie(id)
     }
 };
